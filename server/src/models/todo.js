0722ef8f65b5130a7f29b3a575c6e30f6ab875bb
MorpheusByte <tahasaidcakmak@icloud.com>
2025-09-16 10:23:54 +0300

    2

---
+++ server/src/models/todo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Express - ToDo Api
+
+const {Schema} = require('mongoose');
+
+
+const todoSchema = new Schema({
+
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        minlenght:3,
+        maxleght: 250
+    },
+
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        minlenght: 5,
+        maxlenght: 250
+        },
+    priority: {
+        type: String,
+        enum: [['Low', 'Medium', 'High'], 'Invalid value. Please enter one of these: "Low", "Medium", "High"'],
+        default: "Medium"
+    }
+    
+},{
+    collection: 'todos',
+    timestamps: true
+})
